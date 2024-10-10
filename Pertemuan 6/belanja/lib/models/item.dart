7ed20eb6c42c7814892ae1030272d9b9b1399f96
Astrid Risa Widiana <astrdrisawww2212@gmail.com>
2024-10-10 11:07:03 +0700

    PERTEMUAN 6 MOBILE

--- Pertemuan 6/belanja/lib/models/item.dart
+++ Pertemuan 6/belanja/lib/models/item.dart
@@ -1,6 +1,19 @@
 class Item {
   String name;
   int price;
+  String img;
+  int stock;
+  int rating;
+  String author;
+  String update;
 
-  Item({required this.name, required this.price});
+  Item({
+    required this.name, 
+    required this.price,
+    required this.img,
+    required this.stock,
+    required this.rating,
+    required this.author,
+    required this.update,
+    });
 }
